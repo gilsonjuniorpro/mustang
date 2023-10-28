c8416592f38d4e05b9c44ae250e976b78b5103c3
gilsonjuniorpro <gilsonjuniorpro@gmail.com>
2023-10-28 11:18:52 -0400

    remove unused files

--- shelby/build.gradle.kts
+++ shelby/build.gradle.kts
@@ -49,7 +49,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.core.ktx)
     implementation(libs.lifecycle.runtime.ktx)
     implementation(libs.activity.compose)
@@ -58,11 +57,4 @@ dependencies {
     implementation(libs.ui.graphics)
     implementation(libs.ui.tooling.preview)
     implementation(libs.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(platform(libs.compose.bom))
-    androidTestImplementation(libs.ui.test.junit4)
-    debugImplementation(libs.ui.tooling)
-    debugImplementation(libs.ui.test.manifest)
 }
